01ee53a4eb84de58018e5262e0b959098423b91e
karin0 <karin0@gmx.com>
2020-01-07 23:47:20 +0800

    QAQ

--- frontend/src/env.js
+++
@@ -1,9 +0,0 @@
-const host = 'http://127.0.0.1:5000/';
-/*
-const host = window.location.protocol + '//' +
-    window.location.hostname +
-    (window.location.port ? ':' + window.location.port + '/' : '/');
-*/
-const IMAGES_PER_PAGE = 50;
-
-export { host, IMAGES_PER_PAGE };
